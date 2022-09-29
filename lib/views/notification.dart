17f3cd47ea9beda810e13fa04bca96a10ceae43a
jxtSamFrimpong <hub4realtil@gmail.com>
2022-09-29 08:32:46 +0000

    addnewperson

--- lib/views/notification.dart
+++ lib/views/notification.dart
@@ -6,6 +6,7 @@ import 'package:state/common/app_text.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -55,10 +56,16 @@ class _NotificationPageState extends State<NotificationPage> {
                               return ListView(
                                 shrinkWrap: true,
                                 children: [
-                                  Text(snapshot.data!['last_edit']),
-                                  Image.network(
-                                    snapshot.data!['filepath'].toString(),
-                                  )
+                                  //Text(snapshot.data!['last_edit']),
+                                  MaterialButton(
+                                      onPressed: () {
+                                        AddPersonAlert(context,
+                                            snapshot.data!['filepath']);
+                                      },
+                                      child: Image.network(
+                                        snapshot.data!['filepath'].toString() +
+                                            '?alt=media&token=2959c4f9-68c5-4d98-ac7f-cf901d72a604',
+                                      ))
                                 ],
                               );
                             } else {
@@ -84,4 +91,62 @@ class _NotificationPageState extends State<NotificationPage> {
       },
     );
   }
+}
+
+AddPersonAlert(context, file_path) {
+  TextEditingController _name = TextEditingController();
+  TextEditingController _relationship = TextEditingController();
+
+  Widget addButton = ElevatedButton(
+      child: Text('Add'),
+      onPressed: () async {
+        if (_name.text.isEmpty && _relationship.text.isEmpty) {
+          Fluttertoast.showToast(msg: 'Please provide details of person');
+        } else {
+          await FirebaseFirestore.instance
+              .collection('addToPie')
+              .doc('newperson')
+              .set({
+            "name": _name.text,
+            "relationship": _relationship.text,
+            "file_path": file_path
+          });
+          Navigator.of(context).pop();
+        }
+      });
+
+  Widget backButton = ElevatedButton(
+      child: Text('Back'),
+      onPressed: () {
+        Navigator.of(context).pop();
+      });
+
+  AlertDialog alert = AlertDialog(
+    title: Text('Add this Person to database'),
+    content: Container(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: Column(children: [
+        TextField(
+          controller: _name,
+          decoration: InputDecoration(
+              labelText: 'Enter Name',
+              hintText: 'What\'s the name of this person'),
+        ),
+        TextField(
+          controller: _relationship,
+          decoration: InputDecoration(
+              labelText: 'Relationship',
+              hintText: 'What\'s your relationship with this person'),
+        )
+      ]),
+    ),
+    actions: [backButton, addButton],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
